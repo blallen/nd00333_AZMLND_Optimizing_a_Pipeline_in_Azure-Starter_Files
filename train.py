becbcbde893dd77da2f75462becc8286637c0b6e
Brandon Allen <ballen@symbotic.com>
2021-06-22 19:22:16 +0000

    format with black and isort

--- train.py
+++ train.py
@@ -1,23 +1,36 @@
-from sklearn.linear_model import LogisticRegression
 import argparse
 import os
 import sys
-import numpy as np
-from sklearn.metrics import mean_squared_error
-import sklearn.metrics as metrics
-import joblib
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import OneHotEncoder
-import pandas as pd
-from azureml.core.run import Run
-from azureml.data.dataset_factory import TabularDatasetFactory
 
 import joblib
+import numpy as np
+import pandas as pd
+import sklearn.metrics as metrics
+from azureml.core.run import Run
+from azureml.data.dataset_factory import TabularDatasetFactory
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
+
 
 def clean_data(data):
     # Dict for cleaning data
-    months = {"jan":1, "feb":2, "mar":3, "apr":4, "may":5, "jun":6, "jul":7, "aug":8, "sep":9, "oct":10, "nov":11, "dec":12}
-    weekdays = {"mon":1, "tue":2, "wed":3, "thu":4, "fri":5, "sat":6, "sun":7}
+    months = {
+        "jan": 1,
+        "feb": 2,
+        "mar": 3,
+        "apr": 4,
+        "may": 5,
+        "jun": 6,
+        "jul": 7,
+        "aug": 8,
+        "sep": 9,
+        "oct": 10,
+        "nov": 11,
+        "dec": 12,
+    }
+    weekdays = {"mon": 1, "tue": 2, "wed": 3, "thu": 4, "fri": 5, "sat": 6, "sun": 7}
 
     # Clean and one hot encode data
     x_df = data.to_pandas_dataframe().dropna()
@@ -41,14 +54,24 @@ def clean_data(data):
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
 
     return x_df, y_df
-    
+
 
 def main():
     # Add arguments to script
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--C', type=float, default=1.0, help="Inverse of regularization strength. Smaller values cause stronger regularization")
-    parser.add_argument('--max_iter', type=int, default=100, help="Maximum number of iterations to converge")
+    parser.add_argument(
+        "--C",
+        type=float,
+        default=1.0,
+        help="Inverse of regularization strength. Smaller values cause stronger regularization",
+    )
+    parser.add_argument(
+        "--max_iter",
+        type=int,
+        default=100,
+        help="Maximum number of iterations to converge",
+    )
 
     args = parser.parse_args()
 
@@ -58,9 +81,9 @@ def main():
 
     x, y = clean_data(ds)
 
-    splits = train_test_split(x, y, train_size = 0.8, random_state = 1)
+    splits = train_test_split(x, y, train_size=0.8, random_state=1)
     x_train, x_test, y_train, y_test = splits
-    
+
     run = Run.get_context()
 
     run.log("Regularization Strength:", np.float(args.C))
@@ -71,12 +94,15 @@ def main():
     accuracy = model.score(x_test, y_test)
     run.log("accuracy", np.float(accuracy))
 
-    AUC = metrics.roc_auc_score(y_test, model.decision_function(x_test), average = "weighted")
+    AUC = metrics.roc_auc_score(
+        y_test, model.decision_function(x_test), average="weighted"
+    )
     run.log("AUC_weighted", np.float(AUC))
 
     # save model so can register it
     os.makedirs("outputs", exist_ok=True)
     joblib.dump(model, "outputs/model.joblib")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
